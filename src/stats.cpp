bf134cd39453a2924dd5904b1d8af29515f0d9b3
antnolang <antnolangos@gmail.com>
2021-03-13 12:29:50 +0100

    [feat] New functions to calculate other features
    Now we can calculate median, quantiles and root mean square

--- src/stats.cpp
+++ src/stats.cpp
@@ -2,6 +2,9 @@
 #include "stats.hpp"
 
 
+static double sum_squares(const double data[], const size_t n);
+static void my_downheap(double * data, const size_t N, size_t k);
+
 
 /* stats_mean, stats_variance, stats_kurtosis and stats_skew copied from GSL GNU
  * with some minor changes
@@ -9,6 +12,7 @@
  * GSL NOTE: we use a recurrence relation in these functions to stably update a 
  *           running value so there aren't any large sums that can overflow
  */
+
 
 double stats_mean(const double data[], const size_t size) {
 	double mean = 0;
@@ -85,3 +89,122 @@ void stats_min_max(
 	}
 }
 
+
+void stats_sort(double * data, const size_t n) {
+	size_t N;
+	size_t k;
+
+	if (n == 0)
+		return;			/* No data to sort */
+
+	/* We have n_data elements, last element is at 'n_data-1', first at
+	   '0' Set N to the last element number. */
+
+	N = n - 1;
+
+	k = N / 2;
+	k++;				/* Compensate the first use of 'k--' */
+	do {
+		k--;
+		my_downheap(data, N, k);
+	} while (k > 0);
+
+	while (N > 0) {
+		/* first swap the elements */
+		double tmp = data[0];
+		data[0] = data[N];
+		data[N] = tmp;
+
+		/* then process the heap */
+		N--;
+
+		my_downheap(data, N, 0);
+	}
+}
+
+
+double stats_median_from_sorted_data(const double sorted_data[], const size_t n) {
+	double median;
+	const size_t lhs = (n - 1) / 2;
+	const size_t rhs = n / 2;
+
+	if (n == 0)
+		return 0.0;
+
+	if (lhs == rhs) {
+		median = sorted_data[lhs];
+	} else {
+		median = (sorted_data[lhs] + sorted_data[rhs])/2.0;
+	}
+
+	return median;
+}
+
+
+double stats_quantile_from_sorted_data(
+    const double sorted_data[], const size_t n, const double f
+) {
+	const double index = f * (n - 1) ;
+	const size_t lhs = (int)index ;
+	const double delta = index - lhs ;
+	double result;
+
+	if (n == 0)
+		return 0.0;
+
+	if (lhs == n - 1) {
+		result = sorted_data[lhs];
+	} else {
+		result =   ((1 - delta) * sorted_data[lhs])
+		         + (delta * sorted_data[(lhs + 1)]);
+	}
+
+	return result ;
+}
+
+
+double stats_rms(
+    const double data_x[], const double data_y[], const double data_z[], 
+    const size_t n
+) {
+	double sum = 0;
+
+	sum += sum_squares(data_x, n);
+	sum += sum_squares(data_y, n);
+	sum += sum_squares(data_z, n);
+
+	return sqrt(sum / (n*3));
+}
+
+
+/*============================= Static functions =============================*/
+
+static double sum_squares(const double data[], const size_t n) {
+	double sum = 0;
+
+	for (size_t i = 0; i < n; i++) {
+		sum += data[i] * data[i];
+	}
+
+	return sum;
+}
+
+static void my_downheap(double * data, const size_t N, size_t k) {
+	double v = data[k];
+
+	while (k <= N / 2) {
+		size_t j = 2 * k;
+
+		if (j < N && data[j] < data[(j + 1)])
+			j++;
+
+		if (!(v < data[j]))		/* avoid infinite loop if nan */
+			break;
+
+		data[k] = data[j];
+
+		k = j;
+	}
+
+	data[k] = v;
+}
--- src/stats.hpp
+++ src/stats.hpp
@@ -19,6 +19,15 @@ double stats_skew(
 void stats_min_max(
     double &min, double &max, const double data[], const size_t size
 );
+void stats_sort(double * data, const size_t n);
+double stats_median_from_sorted_data(const double sorted_data[], const size_t n);
+double stats_quantile_from_sorted_data(
+    const double sorted_data[], const size_t n, const double f
+);
+double stats_rms(
+    const double data_x[], const double data_y[], const double data_z[], 
+    const size_t n
+);
 
 
 #endif
